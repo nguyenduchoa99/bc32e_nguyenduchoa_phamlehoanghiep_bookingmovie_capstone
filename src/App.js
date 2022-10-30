2e1b8042d1966c93805f611314a750f710664cf5
nguyenduchoa99 <hoarowa22df@gmail.com>
2022-10-30 12:23:21 +0700

    upload code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {Suspense} from 'react';
+import { Route,Routes } from 'react-router-dom';
+import {Spin} from 'antd';
+
+function App() {
+  return (
+    <Suspense>
+      <Routes>
+        <Route>
+          
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default App;
